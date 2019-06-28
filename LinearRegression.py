a7ef3b2777aabdd89a6a9fb125c16751b54a8f5d
unknown <chuan.khuna@gmail.com>
2019-06-28 09:39:30 +0700

    Add contour plot

--- LinearRegression.py
+++ LinearRegression.py
@@ -67,6 +67,40 @@ class LinearRegression:
         plt.grid(True, alpha=0.5)
         plt.show()
 
+    def contour_plot(self):
+        # prepare data for contour plot
+        parameters = np.array(self.parameters)
+        w0 = parameters[:, 0]
+        w1 = parameters[:, 1]
+        w0_min,w0_max = np.amin(w0),np.amax(w0)
+        w1_min,w1_max = np.amin(w1),np.amax(w1)
+        costs = np.array(self.costs)
+        # contour plot coordinate
+        grid_res = 100
+        x0 = np.linspace(w0_min/2, w0_max*2, grid_res)
+        x1 = np.linspace(w1_min/2, w1_max*2, grid_res)
+        X0, X1 = np.meshgrid(x0, x1)
+        cost_grid = np.zeros(shape=(x0.size, x1.size))
+        levels = np.unique(np.sort(costs))
+
+        # calculate cost grid
+        for i, v0 in enumerate(x0):
+            for j, v1 in enumerate(x1):
+                predicted = self.x*v1 + v0
+                cost = np.sum((predicted - self.expected_y)**2)
+                cost = round(0.5*cost/len(predicted), 1)
+                cost_grid[i][j] = cost
+
+        # contour line
+        # contour = plt.contour(X0, X1, cost_grid, levels, cmap=plt.get_cmap('cool_r'), alpha=0.8)
+        contour = plt.contourf(x0, x1, cost_grid, levels,)
+
+        # plot parameters
+        plt.scatter(w0, w1)
+        plt.xlabel('w0')
+        plt.ylabel('w1')
+        plt.show()
+
 
 if __name__ == "__main__":
     num_sample = 100
@@ -84,3 +118,4 @@ if __name__ == "__main__":
     lr.linear_regression()
     lr.plot_cost_iteration()
     lr.plot_scatter_model()
+    lr.contour_plot()
