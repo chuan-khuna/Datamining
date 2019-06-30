5d9d3dee9d70fd2f385f4ddfe4f26da87b76b10d
Chuan Khuna <chuan.khuna@gmail.com>
2019-06-30 18:07:45 +0700

    Update crane problem

--- container_crane.py
+++ container_crane.py
@@ -7,14 +7,18 @@ csv_data = pd.read_csv('Container_Crane_Controller_Data_Set.csv')
 x = np.array(csv_data[['Speed', 'Angle']])
 y = np.array(csv_data['Power'])
 
-max_iteration = 1000
-learning_rate = 0.001
-initial_params = np.ones(x.shape[1] + 1)
+# insert 1 at column 0
+x = np.insert(x, 0, values=1, axis=1)
+
+max_iteration = 10000
+learning_rate = 0.0001
+initial_params = np.ones(x.shape[1])
 
 lr = LR(x, y, initial_params=initial_params, 
             learning_rate=learning_rate, 
             max_iteration=max_iteration)
 
+print("x", x)
 print("Expected y:   {}".format(y))
 print("Hypothesis y: {}".format(
         np.round(lr.predicted_y, 1)
