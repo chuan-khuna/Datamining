9a88a1334b2fa3848bb65dcd707f7c4fac3f585a
unknown <chuan.khuna@gmail.com>
2019-07-05 22:55:58 +0700

    Add Linear Random bias

--- bias_variance_simulation.py
+++ bias_variance_simulation.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
-
-x = np.arange(0, 2*np.pi, 0.001)
+sin_sample = 1000
+x = np.linspace(0, 2*np.pi, sin_sample)
 y = np.sin(x)
 
 sample = 10
@@ -11,34 +12,66 @@ rand_const = np.random.uniform(-1, 1, sample)
 print("------ constant model ------")
 print("random {} samples constant".format(sample))
 
-# for c in rand_const:
-#     plt.plot(x, [c]*len(x), alpha=0.5)
+for c in rand_const:
+    plt.plot(x, [c]*len(x), alpha=0.2)
 
 # g bar
-rand_mean = round(np.mean(rand_const), 5)
-print("random constant mean: {}".format(rand_mean))
+const_g_bar = round(np.mean(rand_const), 5)
+print("random constant mean: {}".format(const_g_bar))
 
 # bias = g bar - fx
-bias = np.array([rand_mean] * len(y)) - y
-bias = np.sum(bias)
-print("bias = {}".format(bias))
+const_bias = np.array([const_g_bar] * len(y)) - y
+const_bias = np.sum(const_bias)
+print("const bias = {}".format(const_bias))
 
 # variance
-variance = np.mean(
-    (rand_const - rand_mean)**2
+cosnt_variance = np.mean(
+    (rand_const - const_g_bar)**2
 )
-print("variance = {}".format(variance))
+print("const variance = {}".format(cosnt_variance))
 
 print("------ linear model ------")
 
-params = []
-for i in range(sample):
-    fi = np.random.randint(0, len(x)-1)
-    si = np.random.randint(0, len(x)-1)
-    wh
+
+# first and second point index
+fi = random.sample(range(sin_sample), sample)
+si = random.sample(range(sin_sample), sample)
+
+models_w = []
+for i in range(len(fi)):
+    x1 = x[fi[i]]
+    y1 = y[fi[i]]
+    while fi[i] == si[i]:
+        # random agian if same index
+        si[i] = random.randint(0, len(fi)-1)
+    x2 = x[si[i]]
+    y2 = y[si[i]]
+    
+    m = (y1 - y2)/(x1 - x2)
+    c = y1 - m*x1
+    model_w = [c, m]
+    models_w.append(model_w)
+
+models_w = np.array(models_w)
+for i in range(len(models_w)):
+    plt.plot(x, x*models_w[i][1]+models_w[i][0], alpha=0.2)
+    pass
+
+# g bar, y_bar = m_bar*x + c_bar
+m_bar = np.mean(models_w[:, 1])
+c_bar = np.mean(models_w[:, 1])
+print("random linear mean: {}x + {}".format(round(m_bar, 3), round(c_bar, 3)))
+# linear g bar
+l_g_bar = m_bar*x + c_bar
+# bias = g bar - fx
+l_bias = np.sum(l_g_bar - y)
+print("linear bias = {}".format(l_bias))
 
 
-plt.plot(x, [rand_mean]*len(x), 'r-', label="mean of uniform random")
-plt.plot(x, y)
+plt.plot(x, [const_g_bar]*len(x), 'r-', label="mean of const random")
+plt.plot(x, m_bar*x + c_bar, 'g-', label="mean of linear random")
+plt.plot(x, y, 'b-')
+plt.axis([0, 2*np.pi, -1.25, 1.25])
 plt.grid(True, alpha=0.8)
+plt.legend()
 plt.show()
