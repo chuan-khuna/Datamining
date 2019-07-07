31eeb465d45edca6a5152186facdf1bffe85e338
unknown <chuan.khuna@gmail.com>
2019-07-07 15:42:02 +0700

    Add Multiclass classification

--- Exercise/gen_multivariate_data.py
+++ Exercise/gen_multivariate_data.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+num_sample = 50
 
 mean = [3, 0]
 cov = [[1, 0], [0, 1]]  # diagonal covariance
 
-x1, y1 = np.random.multivariate_normal(mean, cov, 25).T
+x1, y1 = np.random.multivariate_normal(mean, cov, num_sample).T
 
 mean = [6.5, 6]
 cov = [[0.3, 0], [0, 0.3]]  # diagonal covariance
 
-x2, y2 = np.random.multivariate_normal(mean, cov, 25).T
+x2, y2 = np.random.multivariate_normal(mean, cov, num_sample).T
 
 mean = [12, 1]
 cov = [[0.5, 0], [0, 1.3]]  # diagonal covariance
 
-x3, y3 = np.random.multivariate_normal(mean, cov, 25).T
+x3, y3 = np.random.multivariate_normal(mean, cov, num_sample).T
 
 plt.plot(x1, y1, 'r.')
 plt.plot(x2, y2, 'g.')
@@ -23,13 +26,13 @@ plt.plot(x3, y3, 'b.')
 x = np.concatenate([
     x1, x2, x3
 ], axis=0)
-
+x = np.round(x, 3)
 y = np.concatenate([
     y1, y2, y3
 ], axis=0)
-
+y = np.round(y, 3)
 out_class = np.concatenate([
-    np.zeros(25), np.ones(25), np.ones(25)*2
+    np.zeros(num_sample), np.ones(num_sample), np.ones(num_sample)*2
 ], axis=0)
 
 my_data = {
@@ -38,4 +41,4 @@ my_data = {
 
 df = pd.DataFrame(my_data, columns= ['x1', 'x2', 'class'])
 df.to_csv('./my_data.csv', index = None, header=True)
-plot.show()
+plt.show()
--- Exercise/my_data_classification.py
+++ Exercise/my_data_classification.py
@@ -10,39 +10,35 @@ df = pd.read_csv("my_data.csv")
 
 x = np.array(df[['x1', 'x2']])
 aug_x = np.insert(x, 0, values=1, axis=1)
-x1 = x[:, 0][0:100]
-x2 = x[:, 1][0:100]
+x1 = x[:, 0]
+x2 = x[:, 1]
 
-y = np.array(df['class'])[0:100]
-
-plt.plot(x1[:50], x2[:50], 'b.')
-plt.plot(x1[50:100], x2[50:100], 'g.')
-plt.plot(x1[100:], x2[100:], 'r.')
-plt.show()
-
+y = np.array(df['class'])
 
 boundaries = []
-for i in range(2):
+for i in range(3):
     # output for classify class i
     yi = np.where(y==i, 1, 0) 
-    print(yi)
     init_w = np.ones(aug_x.shape[1])
     learning_rate = 0.2
-    max_iteration = 2000
+    max_iteration = 200
     lc = LC(aug_x, yi, init_w, learning_rate, max_iteration)
     boundaries.append(lc.parameters[-1])
     print(f'boundary for classify class {i}: {lc.parameters[-1]}')
 
-
+x_val = np.arange(np.amin(x1)-5, np.amax(x1)+5, 0.1)
 for i in range(len(boundaries)):
-    w0, w1, w2 = boundaries[i][0], boundaries[i][1], boundaries[i][2]
-    x_val = np.arange(-5, 10, 0.1)
-    y_val = -(-w1*x_val + w0)/w2
+    w0, w1, w2 = boundaries[i][0], boundaries[i][1], boundaries[i][2]  
+    y_val = -(w1*x_val + w0)/w2
     plt.plot(x_val, y_val, label=f'b{i+1}, {w1:.2f}x1 + {w2:.2f}x2 + {w0:.2f} = 0')
 
-plt.plot(x1[:50], x2[:50], 'bo', label='c1')
-plt.plot(x1[50:100], x2[50:100], 'go', label='c2')
-plt.plot(x1[100:], x2[100:], 'yo', label='c3')
+
+plt.plot(x1[:50], x2[:50], 'b.', label='c1')
+plt.plot(x1[50:100], x2[50:100], 'g.', label='c2')
+plt.plot(x1[100:], x2[100:], 'r.', label='c3')
+plt.axis(
+    [np.amin(x1)-1, np.amax(x1)+1,np.amin(x2)-1, np.amax(x2)+1]
+)
 plt.legend()
 plt.grid(True, alpha=0.5)
 plt.show()
