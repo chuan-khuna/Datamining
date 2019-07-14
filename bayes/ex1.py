8a7707f38d407b85cbae3983f6bbb7ca4af0f6c5
Chuan Khuna <chuan.khuna@gmail.com>
2019-07-14 20:57:17 +0700

    function

--- bayes/ex1.py
+++ bayes/ex1.py
@@ -5,28 +5,43 @@ import seaborn as sns
 import scipy as sp
 
 sns.set_style("whitegrid")
-sns.set_palette("muted")
-sns.set_context('notebook', font_scale=1.25)
+sns.set_palette("bright")
+sns.set_context('notebook', font_scale=1.25, rc={"lines.linewidth": 3})
+
+def likelihood_plot(x, y1, y2):
+    x_lim = [-5, 5]
+    y_lim = [-0.02, 0.5]
+    fig, ax = plt.subplots(figsize=(9, 6))
+    sns.lineplot(x, y1, label='c1')
+    sns.lineplot(x, y2, label='c2')
+    plt.ylabel('likelihood')
+    ax.set_xlim(x_lim)
+    ax.set_ylim(y_lim)
+    plt.legend()
+    plt.show()
+
+def posterior_plot(x, y1, y2):
+    x_lim = [-5, 5]
+    y_lim = [-0.1, 1.1]
+    fig, ax = plt.subplots(figsize=(9, 6))
+    post1 = y1 / (y1+y2)
+    post2 = 1 - post1
+    plt.plot(x, post1, label='c1')
+    plt.plot(x, post2, label='c2')
+    plt.ylabel('posterior prob.')
+    ax.set_xlim(x_lim)
+    ax.set_ylim(y_lim)
+    plt.legend()
+    plt.show()
+
 
 mu1 = -1
 mu2 = 1
 sigma1 = 1
 sigma2 = 1
-num_sample = 500
-
-
 num_sample = 200
 x = np.linspace(-10, 10, num_sample)
-
 y1 = sp.stats.norm.pdf(x, mu1, sigma1)
 y2 = sp.stats.norm.pdf(x, mu2, sigma2)
-
-fig, ax = plt.subplots(figsize=(9, 6))
-sns.lineplot(x, y1, label='c1')
-sns.lineplot(x, y2, label='c2')
-plt.ylabel('likelihood')
-ax.set_xlim([-5, 5])
-ax.set_ylim([-0.02, 0.5])
-plt.legend()
-plt.show()
-
+likelihood_plot(x, y1, y2)
+posterior_plot(x, y1, y2)
