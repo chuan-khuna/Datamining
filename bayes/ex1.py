ab3ba2d2185397c7223f9181fb14502befe375fc
Chuan Khuna <chuan.khuna@gmail.com>
2019-07-22 10:16:27 +0700

    Update ex1 and ex2

--- bayes/ex1.py
+++ bayes/ex1.py
@@ -16,14 +16,14 @@ sigma1 = 1
 sigma2 = 1
 num_sample = 200
 boundary = b.single_threshold_boundary(mu1, mu2)
-x = np.linspace(-10, 10, num_sample)
+x = np.linspace(-15, 15, num_sample)
 x_lim = [-5, 5]
 y1 = sp.stats.norm.pdf(x, mu1, sigma1)
 y2 = sp.stats.norm.pdf(x, mu2, sigma2)
-b.likelihood_plot(x, y1, y2, boundary, x_lim)
-b.posterior_plot(x, y1, y2, boundary, x_lim)
+# b.likelihood_plot(x, y1, y2, boundary, x_lim)
+# b.posterior_plot(x, y1, y2, boundary, x_lim)
 
-rand_size = 50
+rand_size = 200
 x1 = np.random.normal(loc=mu1, scale=sigma1, size=rand_size)
 x2 = np.random.normal(loc=mu2, scale=sigma2, size=rand_size)
 sns.distplot(x1)
@@ -31,8 +31,8 @@ sns.distplot(x2)
 mu1 = np.mean(x1)
 mu2 = np.mean(x2)
 boundary = b.single_threshold_boundary(mu1, mu2)
-sigma1 = np.sum((x1 - mu1)**2)/len(x1)
-sigma1 = np.sum((x2 - mu2)**2)/len(x2)
+sigma1 = np.sqrt(np.sum((x1 - mu1)**2)/len(x1))
+sigma2 = np.sqrt(np.sum((x2 - mu2)**2)/len(x2))
 print(f'mu1: {mu1:.2f}, sigma1: {sigma1:.2f}')
 print(f'mu2: {mu2:.2f}, sigma2: {sigma2:.2f}')
 y1 = sp.stats.norm.pdf(x, mu1, sigma1)
--- bayes/ex2.py
+++ bayes/ex2.py
@@ -18,5 +18,23 @@ x = np.linspace(-15, 15, num_sample)
 x_lim = [-15, 15]
 y1 = sp.stats.norm.pdf(x, mu1, sigma1)
 y2 = sp.stats.norm.pdf(x, mu2, sigma2)
+# b.likelihood_plot(x, y1, y2, boundary, x_lim, [-0.01, 0.3])
+# b.posterior_plot(x, y1, y2, boundary, x_lim)
+
+
+rand_size = 200
+x1 = np.random.normal(loc=mu1, scale=sigma1, size=rand_size)
+x2 = np.random.normal(loc=mu2, scale=sigma2, size=rand_size)
+sns.distplot(x1)
+sns.distplot(x2)
+mu1 = np.mean(x1)
+mu2 = np.mean(x2)
+sigma1 = np.sqrt(np.sum((x1 - mu1)**2)/len(x1))
+sigma2 = np.sqrt(np.sum((x2 - mu2)**2)/len(x2))
+boundary = b.double_threshold_boundary(mu1, mu2, sigma1, sigma2)
+print(f'mu1: {mu1:.2f}, sigma1: {sigma1:.2f}')
+print(f'mu2: {mu2:.2f}, sigma2: {sigma2:.2f}')
+y1 = sp.stats.norm.pdf(x, mu1, sigma1)
+y2 = sp.stats.norm.pdf(x, mu2, sigma2)
 b.likelihood_plot(x, y1, y2, boundary, x_lim, [-0.01, 0.3])
 b.posterior_plot(x, y1, y2, boundary, x_lim)
