35235461258bbf13d6d2a46a25d32dcfe3bad96b
Chuan Khuna <chuan.khuna@gmail.com>
2019-07-22 15:28:57 +0700

    Add Ex4

--- bayes/ex4.py
+++ bayes/ex4.py
@@ -26,35 +26,28 @@ prob_c1 = 0.5
 
 # c2
 mu2 = [-2, 0]
-sigma2 = [[1, 0], [0, 2]]
+sigma2 = sigma1
 z2 = sp.stats.multivariate_normal.pdf(xy, mean=mu2, cov=sigma2)
 prob_c2 = 0.5
+
+mat_mu1 = Matrix(mu1).T
+mat_mu2 = Matrix(mu2).T
+mat_sigma = Matrix(sigma1)
+x1, x2 = symbols('x1 x2')
+mat_x = matrix_x = Matrix([x1, x2]).T
 
 b.surface_plot(x, y, [z1, z2])
 b.contour_plot(x, y, [z1, z2])
 
-x1, x2 = symbols('x1 x2')
-matrix_x = Matrix([x1, x2]).T
-matrix_mu1 = Matrix(mu1).T
-matrix_sigma1 = Matrix(sigma1)
-matrix_mu2 = Matrix(mu2).T
-matrix_sigma2 = Matrix(sigma2)
-# posterior = z1/(z1+z2)
+g = b.g_lin(mat_mu1, mat_mu2, mat_sigma, mat_x, 0.5, 0.5)
 
-g1 = b.g_i(1, matrix_x, matrix_mu1, matrix_sigma1, prob_c=prob_c1)
-g2 = b.g_i(1, matrix_x, matrix_mu2, matrix_sigma2, prob_c=prob_c2)
-g = g1 - g2
-
-print('g1 =', g1)
-print('g2 =', g2)
-print('g1 - g2 =', g)
+print('g =', g)
 g = solve(g, x2)
 lamb_g = lambdify(x1, g, 'numpy')
 
 x_val = np.arange(-10, 10, 0.001)
 y_val = lamb_g(x_val)
 plt.plot(x_val, y_val[0], 'b')
-plt.plot(x_val, y_val[1], 'b')
 plt.contour(x, y, z1)
 plt.contour(x, y, z2)
 plt.contourf(x, y, z1/(z1+z2), alpha=0.75)
